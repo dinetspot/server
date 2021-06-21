0c80eedd1486646fcca9b41ed818f7dbbf5a3e61
dinetspot <86222556+dinetspot@users.noreply.github.com>
2021-06-21 13:58:29 +0700

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,6 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<dbl403t','mikhmon>|>aGJibmFq');
+
+
+$data['dinet01'] = array ('1'=>'dinet01!192.168.1.1','dinet01@|@dblaoet','dinet01#|#aGJibmFq','dinet01%DInet_Hotspot','dinet01^dinet.net','dinet01&Rp','dinet01*10','dinet01(1','dinet01)','dinet01=10','dinet01@!@disable');
